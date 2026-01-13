fcb9950b55b415046942ac3bccd2f7c27bd1b6f0
AndrewLicona <andrewlicona70@gmail.com>
2026-01-13 14:46:13 -0500

    fix: set global api prefix and strict cors origins

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,7 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://shopylink.andrewlamaquina.my',
+      'https://andrewlamaquina.my',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
